22245889cbe5089313c031c00b6f20529ceab1e8
LuliBobo <99192323+LuliBobo@users.noreply.github.com>
2023-09-23 16:38:00 +0200

    Translate to Slovak

--- scratch.py
+++ scratch.py
@@ -1,4 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from translate import Translator
 
 try:
     # Get the video URL or video ID from the user
@@ -10,10 +11,17 @@ try:
     # Create or overwrite the "subtitles.txt" file
     with open("subtitles.txt", "w", encoding="utf-8") as f:
         for entry in transcript:
-            # Write each entry in the transcript to the file
+            # Translate the text to Slovak
             text = entry["text"]
-            f.write(f"{text}\n")
+            translator = Translator(to_lang="sk")
+            translated_text = translator.translate(text)
 
-    print("Transcript has been saved to 'subtitles.txt'")
+            # Write the translated text to the file
+            f.write(f"{translated_text}\n")
+
+    print("Transcript has been translated to Slovak and saved to 'subtitles.txt'")
 except Exception as e:
     print(f"An error occurred: {str(e)}")
+
+
+
